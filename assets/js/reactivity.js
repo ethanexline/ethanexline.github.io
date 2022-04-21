ff177c1da163662c1a53db85f18c923d90cddc69
Ethan Exline <93214868+ethanexline@users.noreply.github.com>
2022-04-21 13:33:12 -0400

    700th time's the charm

--- assets/js/reactivity.js
+++ assets/js/reactivity.js
@@ -255,7 +255,7 @@ function toggleTheme() {
         theme[3].setAttribute('media', 'none');
         bodyPic[0].setAttribute('style', "overflow: hidden; font-family: Tahoma, sans-serif; min-height: 100%; background-repeat: no-repeat; background-attachment: fixed; " +
             "background-position: center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; " +
-            "background-size: " + $(window).width + "px " + $(window).height + "px; background-color: white; background-image: url('assets/images/win98.webp'); " +
+            "background-size: " + window.innerWidth + "px " + window.innerHeight + "px; background-color: white; background-image: url('assets/images/win98.webp'); " +
             "max-width: 100vw;");
         startBar.style.display = "flex";
         startBar.className = "";
@@ -308,7 +308,7 @@ function toggleTheme() {
         theme[3].setAttribute('media', 'none');
         bodyPic[0].setAttribute('style', "overflow: hidden; font-family: 'Franklin Gothic', sans-serif; min-height: 100%; background-repeat: no-repeat; " +
             "background-attachment: fixed; background-position: center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; " +
-            "background-size: cover; background-size: " + $(window).width + "px " + $(window).height + "px; background-color: white; " +
+            "background-size: cover; background-size: " + window.innerWidth + "px " + window.innerHeight + "px; background-color: white; " +
             "background-image: url('assets/images/bliss.webp'); max-width: 100vw;");
         startBar.style.display = "flex";
         startBar.className = "";
@@ -361,7 +361,7 @@ function toggleTheme() {
         theme[3].setAttribute('media', 'none');
         bodyPic[0].setAttribute('style', "overflow: hidden; font-family: 'Segoe UI', sans-serif; min-height: 100%; background-repeat: no-repeat; background-attachment: fixed; " +
             "background-position: center; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; " +
-            "background-size: " + $(window).width + "px " + $(window).height + "px; background-color: white; background-image: url('assets/images/win7default.webp'); max-width: 100vw;");
+            "background-size: " + window.innerWidth + "px " + window.innerHeight + "px; background-color: white; background-image: url('assets/images/win7default.webp'); max-width: 100vw;");
         startBar.style.display = "flex";
         startBar.className = "";
         startBar.className = "seven window glass";
